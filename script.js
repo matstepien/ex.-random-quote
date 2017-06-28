09617260b9b3eb2b5d2b58b39b1602c7e429f5a6
Mateusz <matstepien@outlook.com>
2017-06-28 11:12:18 +0200

    random quote

---
+++ script.js
@@ -0,0 +1,36 @@
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+	quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+
+function getQuote() {
+    $.getJSON(quoteUrl, createTweet);
+}
+
+function createTweet(input) {
+	var data = input[0];
+		quoteText = $(data.content).text().trim();
+		quoteAuthor = data.title;
+
+	if (!quoteAuthor.length) {
+		quoteAuthor = "Unknown author";
+	}
+
+	var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+	if (tweetText.length > 140) {
+    	getQuote();
+	} else {
+		var tweet = tweetLink + encodeURIComponent(tweetText);
+		$('.quote').text(quoteText);
+		$('.author').text("Author: " + quoteAuthor);
+		$('.tweet').attr('href', tweet);
+	}
+
+	$('.tweet').attr('href', tweet);
+}
+
+$(document).ready(function() {
+    getQuote();
+    $('.trigger').click(function() {
+        getQuote();
+    })
+});
